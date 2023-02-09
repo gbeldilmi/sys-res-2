63ee231e5ea1aa65cd57baa4336d77528839e940
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-02-09 18:15:51 +0100

    Add DHCP server setup

--- server/setup.sh
+++ server/setup.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# Install packages
+apt install -y tree vim htop isc-dhcp-server
+
 # Set up the network interface
 cp resources/interfaces /etc/network/interfaces
 systemctl restart networking.service
@@ -7,3 +10,8 @@ systemctl restart networking.service
 # Set up ssh
 cp resources/sshd_config /etc/ssh/sshd_config
 systemctl enable sshd.service
+
+# Set up dhcp server
+cp resources/iscdhcp-server /etc/default/isc-dhcp-server
+cp resources/dhcpd.conf /etc/dhcp/dhcpd.conf
+service isc-dhcp-server restart
