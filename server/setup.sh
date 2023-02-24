d4665d848c41177fdb2911f36c5a3a912cd28d77
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-02-24 15:54:20 +0100

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -5,8 +5,8 @@ apt install -y isc-dhcp-server iptables net-tools
 
 
 # Set up the network interface
-cp resources/interfaces /etc/network/interfaces
-cp resources/sysctl.conf /etc/sysctl.conf
+cp ./resources/interfaces /etc/network/interfaces
+cp ./resources/sysctl.conf /etc/sysctl.conf
 sysctl -p
 systemctl restart networking.service
 
@@ -19,7 +19,7 @@ iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 iptables -A FORWARD -ptcp --dport http -i enp3s0 -o eno1 -j ACCEPT
 iptables -A FORWARD -ptcp --dport https -i enp3s0 -o eno1 -j ACCEPT
@@ -36,13 +36,13 @@ iptables-save > /etc/iptables/rules.v4
 
 
 # Set up ssh
-cp resources/sshd_config /etc/ssh/sshd_config
+cp ./resources/sshd_config /etc/ssh/sshd_config
 systemctl enable sshd.service
 
 
 # Set up dhcp server
-cp resources/iscdhcp-server /etc/default/isc-dhcp-server
-cp resources/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp ./resources/iscdhcp-server /etc/default/isc-dhcp-server
+cp ./resources/dhcpd.conf /etc/dhcp/dhcpd.conf
 service isc-dhcp-server restart
 
 
